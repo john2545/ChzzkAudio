010a1ff28ea262f50dbae507eb2f4d6d01c36f96
john2545 <47453953+john2545@users.noreply.github.com>
2024-01-08 00:46:31 +0900

    Update ChzzkAudio.py

--- ChzzkAudio.py
+++ ChzzkAudio.py
@@ -2,9 +2,9 @@ import streamlit as st
 import requests
 import m3u8
 import json
-real_username, liveCategoryValue, liveCategory = None, None, None
+live_status, real_username, liveCategoryValue, liveCategory, userAdultStatus = None, None, None, None, None
 def get_stream_url(username='룩삼오피셜'):
-    global real_username,liveCategoryValue, liveCategory
+    global real_username,liveCategoryValue, liveCategory, live_status
     req_result = requests.get(f"https://api.chzzk.naver.com/service/v1/search/channels?keyword={username}")
     channelId = req_result.json()['content']['data'][0]['channel']['channelId']
     if channelId:
@@ -14,6 +14,10 @@ def get_stream_url(username='룩삼오피셜'):
         liveCategoryValue = content['liveCategoryValue']
         liveCategory = content['liveCategory']
         live_status = content['status']
+        userAdultStatus = content["userAdultStatus"]
+        if live_status == "OPEN":
+            if  == "NOT_LOGIN_USER":
+                st.write("성인방송")
         if live_status == "OPEN":
             status_message = 'open'
             video_m3u8 = json.loads(content['livePlaybackJson'])['media'][0]['path']
@@ -29,19 +33,25 @@ st.title("Chzzk Audio Finder")
 username = st.text_input("Enter Chzzk username:", value='룩삼오피셜', placeholder='치지직 닉네임')
 
 if username:
-    try:
-        title, stream_url_low, stream_url_high = get_stream_url(username)
-        if stream_url_low and stream_url_high:
-            st.success(f"Stream found")
-            st.write(f"닉네임: {real_username}")
-            st.write(f"제목: {title}")
-            st.write(f"카테고리: {liveCategoryValue}({liveCategory})")
-            st.write(f'Low(AAC 64kbps): {stream_url_low}')
-            st.write(f'High(AAC 192kbps): {stream_url_high}')
-            st.write("둘 중 하나를 복사하여 플레이어(VLC, 팟플레이어 등)로 재생하세요.")
-        else:
-            st.error("Stream not found.")
-            st.write(status_message)
+    if live_status == "OPEN":
+        try:
+            title, stream_url_low, stream_url_high = get_stream_url(username)
+            if stream_url_low and stream_url_high:
+                st.success(f"Stream found")
+                st.write(f"닉네임: {real_username}")
+                st.write(f"제목: {title}")
+                st.write(f"카테고리: {liveCategoryValue}({liveCategory})")
+                st.write(f'Low(AAC 64kbps): {stream_url_low}')
+                st.write(f'High(AAC 192kbps): {stream_url_high}')
+                st.write("둘 중 하나를 복사하여 플레이어(VLC, 팟플레이어 등)로 재생하세요.")
+            else if userAdultStatus == "NOT_LOGIN_USER":
+                st.write(f"닉네임: {real_username}")
+                st.write(f"제목: {title}")
+                st.write(f"카테고리: {liveCategoryValue}({liveCategory})")
+                st.write("성인인증이 필요한 방송입니다. 오디오 주소를 가져올 수 없습니다.")
+            else:
+                st.error("Stream not found.")
+                st.write(status_message)
     except:
         pass
 else:
