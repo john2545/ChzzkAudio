a7c2a12e5fe95939fab5ae0aa14a20f697920292
john2545 <47453953+john2545@users.noreply.github.com>
2024-01-07 18:47:09 +0900

    Update ChzzkAudio.py

--- ChzzkAudio.py
+++ ChzzkAudio.py
@@ -8,14 +8,16 @@ def get_stream_url(username='룩삼오피셜'):
     req_result = requests.get(f"https://api.chzzk.naver.com/service/v1/search/channels?keyword={username}")
     channelId = req_result.json()['content']['data'][0]['channel']['channelId']
     if channelId:
-        content = requests.get(f"https://api.chzzk.naver.com/service/v2/channels/{channelId}/live-detail").json()['content']
+        live_detail = requests.get(f"https://api.chzzk.naver.com/service/v2/channels/{channelId}/live-detail").json()
+        content = live_detail['content']
         real_username = content['channel']['channelName']
+        title = live_detail['liveTitle']
         live_status = content['status']
         if live_status == "OPEN":
             status_message = 'open'
             video_m3u8 = json.loads(content['livePlaybackJson'])['media'][0]['path']
             playlists = m3u8.load(video_m3u8)
-            return playlists.media[1].base_uri+playlists.media[1].uri, playlists.media[0].base_uri+playlists.media[0].uri
+            return title, playlists.media[1].base_uri+playlists.media[1].uri, playlists.media[0].base_uri+playlists.media[0].uri
         else:
             status_message = 'close'
             st.write(f'{real_username}은 방송 중이 아닙니다.')
@@ -27,9 +29,9 @@ username = st.text_input("Enter Chzzk username:", value='룩삼오피셜', place
 
 if username:
     try:
-        stream_url_low, stream_url_high = get_stream_url(username)
+        title, stream_url_low, stream_url_high = get_stream_url(username)
         if stream_url_low and stream_url_high:
-            st.success(f"Stream found: content")
+            st.success(f"Stream found: title")
             st.write(f'Low(AAC 64kbps): {stream_url_low}')
             st.write(f'High(AAC 192kbps): {stream_url_high}')
             st.write("둘 중 하나를 복사하여 플레이어(VLC, 팟플레이어 등)로 재생하세요.")
